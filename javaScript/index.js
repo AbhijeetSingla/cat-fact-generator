309442e43958229cdd160ccdf0faf908cda96767
Abhijeet Singla <95906204+AbhijeetSingla@users.noreply.github.com>
2022-04-20 12:14:07 +0530

    fix: word limit now working

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,14 +1,15 @@
-let wordLimit = document.querySelector("wordlimit");
+let wordLimit = document.querySelector("#wordlimit").value;
 let factOutput = document.querySelector("#factoutput");
 let initialCatApiUrl = `https://catfact.ninja/fact`;
 let catApiUrlComplete = "";
 
 function generateFact() {
-    wordLimit = document.querySelector("wordlimit");
-    if (wordLimit === null) {
+    wordLimit = document.querySelector("#wordlimit").value;
+    if (wordLimit === "") {
         catApiUrlComplete = initialCatApiUrl;
-    } else {catApiUrlComplete = `${initialCatApiUrl}?max_length=${wordLimit.value}`}
-    console.log(catApiUrlComplete);
+    } else {
+        catApiUrlComplete = `${initialCatApiUrl}?max_length=${wordLimit}`;
+    }
     fetch(catApiUrlComplete)
     .then(response => response.json())
     .then(data => factOutput.innerText = data.fact)
